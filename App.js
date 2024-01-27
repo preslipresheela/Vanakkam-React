d3e05dcd35078717275ba32f204d3c509a21136d
preslipresheela <presheelalasar@gmail.com>
2024-01-27 10:21:10 +0530

    committing all the files from Vanakkam-React

---
+++ App.js
@@ -0,0 +1,15 @@
+
+/*
+<div>
+ <div>
+   <h1 id="heading1">h1 tag</h1>
+   <h2 id="heading2">h2 tag</h2>
+ </div>  
+</div>
+*/
+
+
+const heading=React.createElement("h1",{id:"heading",xyz:"xyz"},"Hello from React");
+const parent=React.createElement("div",{},React.createElement("div",{},[React.createElement("h1",{},"h1 tag"),React.createElement("h2",{},"h2 tag")]));
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
